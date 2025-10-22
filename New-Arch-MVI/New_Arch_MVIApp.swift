3d6447b2530834aaaa10910c28b9c49df284333d
Tornike Bardadze <tbardadze@bog.ge>
2025-10-23 01:46:29 +0400

    Initial Commit

---
+++ New-Arch-MVI/New_Arch_MVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Arch_MVIApp.swift
+//  New-Arch-MVI
+//
+//  Created by Tornike Bardadze on 23.10.25.
+//
+
+import SwiftUI
+
+@main
+struct New_Arch_MVIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
